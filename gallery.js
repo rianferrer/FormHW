b5110acab3815cde099f08e88b53bc4b524bc6bb
Marion Ferrer <marioncaryl.ferrer.iics@ust.edu.ph>
2020-02-13 18:38:39 +0800

    fourth commit

--- gallery.js
+++ gallery.js
@@ -1,12 +1,13 @@
-function billingFunction() {
-  if (document.getElementById('same').checked)
+function billingFunction()
+{
+  if (document.getElementById("same").checked)
   {
-    document.getElementById('billingName').value = document.getElementById('shippingName').value;
-    document.getElementById('billingZip').value = document.getElementById('shippingZip').value;
+    document.getElementById("billingName").value = document.getElementById("shippingName").value;
+    document.getElementById("billingZip").value = document.getElementById("shippingZip").value;
   }
   else
   {
-    document.getElementById('billingName').value = null;
-    document.getElementById('billingZip').value = null;
+    document.getElementById("billingName").value = "";
+    document.getElementById("billingZip").value = "";
   }
 }
